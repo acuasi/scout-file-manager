3824af9e619ba8c5f81eb2a7272a318d83c9a255
rayjanwilson <rayjan.wilson@gmail.com>
2012-11-10 00:27:28 -0800

    added the load_flight_log() function. pulls in a yaml formatted flight log and returns a dictionary

--- transfer.py
+++ transfer.py
@@ -5,10 +5,37 @@
 # into the meaningful folder hierarchy
 
 import os
+import re
 import shutil
+import yaml
+import pprint
 
 source_dir = "/Volumes/Storage/IPC-test/85/2012-11-07"
 dest_dir = "/Volumes/Storage/IPC-test/IPC"
+pp = pprint.PrettyPrinter()
+
+def load_flight_log(yaml_file):
+    # load in a yaml file as a dictionary
+    # used this link: http://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries-in-python
+    try:
+        if os.path.exists(yaml_file):
+            file_handle = open(yaml_file)
+            flight_log = yaml.safe_load(file_handle)
+            file_handle.close()
+    except IOError:
+        print 'cannot open', yaml_file
+        raise
+
+    pp.pprint(flight_log)
+    return flight_log
+
+def parse_filename(file_name):
+    # 2012-11-07-flight_007-050.dng
+    [year, month, day, flight_str, number] = re.split('\-', file_name)
+    date = year + '-' + month + '-' + day
+    flight_no = int(re.split('\_', flight_str)[1])
+    return date, flight_no
+
 
 def make_dest_dirs(dest_dir):
     if(os.path.isdir(dest_dir)):
@@ -68,6 +95,12 @@ def make_dest_dirs(dest_dir):
 
 def main():
     make_dest_dirs(dest_dir)
+    file_name = '2012-11-07-flight_007-050.dng'
+    [date, flight] = parse_filename(file_name)
+    print date
+    print flight
+
+    load_flight_log('flight_log.yml')
 
 if __name__ == '__main__':
     main()
